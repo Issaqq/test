34fe18640b90cbd8cd8f50f304b0d25d0afea979
Issukun <136952514+Issaqq@users.noreply.github.com>
2024-12-03 20:57:36 +0530

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello():
     return "<h1>Hello, World!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render's dynamically assigned port
+    app.run(host="0.0.0.0", port=port)        # Bind to all interfaces (0.0.0.0)
